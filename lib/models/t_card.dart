2f94a9a5650708a6431b8e0855f22f14f96df7ba
M-Shinoda <sect011masami@gmail.com>
2022-04-21 12:09:49 +0900

    add due paramater from json in card

--- lib/models/t_card.dart
+++ lib/models/t_card.dart
@@ -5,13 +5,18 @@ class TCard {
   final String name;
   final num position;
   final List<Label> labels;
+  final String? dueDate;
+  final bool dueComplete;
 
-  TCard(this.id, this.name, this.position, this.labels);
+  TCard(this.id, this.name, this.position, this.labels, this.dueDate,
+      this.dueComplete);
 
   TCard.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         name = json['name'],
         position = json['pos'],
+        dueDate = json['due'],
+        dueComplete = json['dueComplete'],
         labels = (json['labels'] as List<dynamic>)
             .map((e) => Label.fromJson(e as Map<String, dynamic>))
             .toList();
