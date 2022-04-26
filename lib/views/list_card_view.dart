ffb05ccd750d82000e339555da1c2f12e5c216ff
M_Shinoda <sect011masami@gmail.com>
2022-04-26 14:31:15 +0900

    add floating button but no function

--- lib/views/list_card_view.dart
+++ lib/views/list_card_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -60,7 +61,9 @@ class CardsView extends HookWidget {
                     ...listCards.value
                         .map((listCard) => listItem(context, listCard))
                   ])))
-                ]))));
+                ])),
+            floatingActionButton: FloatingActionButton(
+                onPressed: () {}, child: const Icon(CupertinoIcons.add))));
   }
 
   Widget listItem(BuildContext context, TCard card) {
